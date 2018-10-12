4eff90472d8dd74ddda3a8e7e3bbc51efd92643c
Peter <peter@anyline.io>
2018-10-12 11:30:14 +0200

    added MRZ, OCR

--- plugin/src/windows/AnylineProxy.js
+++ plugin/src/windows/AnylineProxy.js
@@ -2,7 +2,17 @@ cordova.commandProxy.add("AnylineSDK",{
     LICENSE_PLATE: function (onSuccess, onError, configArray) {
         const license = configArray[0];
         const viewConfig = configArray[1];
+        window.anyline.sdk.init(license, 'LICENSE_PLATE', viewConfig, onSuccess, onError, "");
+    },
+    ANYLINE_OCR: function (onSuccess, onError, configArray) {
+        const license = configArray[0];
+        const viewConfig = configArray[1];
         const ocrConfig = configArray[2];
         window.anyline.sdk.init(license, 'LICENSE_PLATE', viewConfig, onSuccess, onError, ocrConfig);
-    }
+    },
+    MRZ: function (onSuccess, onError, configArray) {
+        const license = configArray[0];
+        const viewConfig = configArray[1];
+        window.anyline.sdk.init(license, 'LICENSE_PLATE', viewConfig, onSuccess, onError, "");
+    },
 });
