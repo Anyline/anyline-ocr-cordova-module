c24e74935117890f7811d493f97d8033b75b63f8
Lorena Krocos <lorena@anyline.io>
2018-11-07 17:56:57 +0100

    update json for energy and fuctions

--- example/www/js/anyline.energy.js
+++ example/www/js/anyline.energy.js
@@ -28,7 +28,7 @@ anyline.energy = {
     }
     var div = document.getElementById('results');
 
-    if(div.childElementCount >= 3) {
+    if (div.childElementCount >= 3) {
       div.removeChild(div.childNodes[div.childElementCount - 1]);
     }
 
@@ -58,66 +58,71 @@ anyline.energy = {
 
     alert(error);
   },
-    
+
   licenseKey: "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
-    
-serialNumber: {
-    "id" : "Meter_ID",
-    "meterPlugin" : {
-        "scanMode" : "SERIAL_NUMBER",
-        "serialNumberCharWhitelist": '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        "serialNumberValidationRegex": '^[0-9A-Z]{5,}$'
-    }
-},
-    
-meterViewPluginConfig: {
-    "camera" : {
-        "captureResolution" : "720p"
-    },
-    "flash" : {
-        "mode": "manual",
-        "alignment": "bottom_right",
-    },
-    "viewPlugin" : {
-        "plugin" : {
-            "id" : "Meter_ID",
-            "meterPlugin" : {
-                "scanMode" : "AUTO_ANALOG_DIGITAL_METER"
-            }
-        },
-        "cutoutConfig" : {
-            "style": "rect",
-            "alignment": "top_half",
-            "strokeWidth": 2,
-            "cornerRadius": 4,
-            "strokeColor": "FFFFFF",
-            "outerColor": "000000",
-            "outerAlpha": 0.3,
-            "feedbackStrokeColor": "0099FF"
-        },
-        "scanFeedback" : {
-            "style": "CONTOUR_RECT",
-            "strokeColor": "0099FF",
-            "fillColor": "220099FF",
-            "blinkOnResult": true,
-            "beepOnResult": true,
-            "vibrateOnResult": true
-        },
-        "cancelOnResult" : true
-    }
-},
 
+  serialNumber: {
+    "id": "Meter_ID",
+    "meterPlugin": {
+      "scanMode": "SERIAL_NUMBER",
+      "serialNumberCharWhitelist": '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      "serialNumberValidationRegex": '^[0-9A-Z]{5,}$'
+    }
+  },
 
-  energyConfigWithSegment: [
-    "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
-    {
-      "captureResolution": "1080p",
-      "visualFeedback": {
+  meterViewPluginConfig: {
+    "camera": {
+      "captureResolution": "720p"
+    },
+    "flash": {
+      "mode": "manual",
+      "alignment": "bottom_right",
+    },
+    "viewPlugin": {
+      "plugin": {
+        "id": "Meter_ID",
+        "meterPlugin": {
+        }
+      },
+      "cutoutConfig": {
+        "style": "rect",
+        "alignment": "top_half",
+        "strokeWidth": 2,
+        "cornerRadius": 4,
+        "strokeColor": "FFFFFF",
+        "outerColor": "000000",
+        "outerAlpha": 0.3,
+        "feedbackStrokeColor": "0099FF"
+      },
+      "scanFeedback": {
         "style": "CONTOUR_RECT",
         "strokeColor": "0099FF",
-        "fillColor": "220099FF"
+        "fillColor": "220099FF",
+        "blinkOnResult": true,
+        "beepOnResult": true,
+        "vibrateOnResult": true
       },
-      "cutout": {
+      "cancelOnResult": true
+    }
+  },
+
+  energyAnalogConfigWithSegment:
+  {
+    "camera": {
+      "captureResolution": "720p"
+    },
+    "flash": {
+      "mode": "manual",
+      "alignment": "bottom_right",
+    },
+    "viewPlugin": {
+      "plugin": {
+        "id": "Meter_ID",
+        "meterPlugin": {
+          "scanMode": "ANALOG_METER"
+        }
+      },
+      "cutoutConfig": {
         "style": "rect",
         "alignment": "top_half",
         "strokeWidth": 2,
@@ -125,40 +130,47 @@ meterViewPluginConfig: {
         "strokeColor": "FFFFFF",
         "outerColor": "000000",
         "outerAlpha": 0.3,
-        "feedbackStrokeColor": "0099FF",
+        "feedbackStrokeColor": "0099FF"
       },
-      "flash": {
-        "mode": "manual",
-        "alignment": "bottom_right"
-      },
-      "beepOnResult": true,
-      "vibrateOnResult": true,
-      "blinkAnimationOnResult": true,
-      "cancelOnResult": true,
-      "reportingEnabled": true,
-      "segment": {
-        "titles": ["Analog", "Digital"],
-        "modes": ["ANALOG_METER", "DIGITAL_METER"],
-        "tintColor": "CCCCCC",
-        "offset": {
-          "x": 0,
-          "y": 400
-        }
-      }
-    },
-    {"nativeBarcodeEnabled": true}
-  ],
-
-  heatConfigWithSegment: [
-    "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
-    {
-      "captureResolution": "1080p",
-      "visualFeedback": {
-        "style": "RECT",
+      "scanFeedback": {
+        "style": "CONTOUR_RECT",
         "strokeColor": "0099FF",
-        "fillColor": "220099FF"
+        "fillColor": "220099FF",
+        "blinkOnResult": true,
+        "beepOnResult": true,
+        "vibrateOnResult": true
       },
-      "cutout": {
+      "cancelOnResult": true,
+      "nativeBarcodeEnabled": true
+    },
+    "segment": {
+      "titles": ["Analog", "Digital"],
+      "modes": ["ANALOG_METER", "DIGITAL_METER"],
+      "tintColor": "CCCCCC",
+      "offset": {
+        "x": 0,
+        "y": 400
+      }
+    }
+  },
+
+  energyDigitalConfigWithSegment:
+  {
+    "camera": {
+      "captureResolution": "720p"
+    },
+    "flash": {
+      "mode": "manual",
+      "alignment": "bottom_right",
+    },
+    "viewPlugin": {
+      "plugin": {
+        "id": "Meter_ID",
+        "meterPlugin": {
+          "scanMode": "DIGITAL_METER"
+        }
+      },
+      "cutoutConfig": {
         "style": "rect",
         "alignment": "top_half",
         "strokeWidth": 2,
@@ -166,34 +178,130 @@ meterViewPluginConfig: {
         "strokeColor": "FFFFFF",
         "outerColor": "000000",
         "outerAlpha": 0.3,
-        "feedbackStrokeColor": "0099FF",
+        "feedbackStrokeColor": "0099FF"
       },
-      "flash": {
-        "mode": "manual",
-        "alignment": "bottom_right"
+      "scanFeedback": {
+        "style": "CONTOUR_RECT",
+        "strokeColor": "0099FF",
+        "fillColor": "220099FF",
+        "blinkOnResult": true,
+        "beepOnResult": true,
+        "vibrateOnResult": true
       },
-      "beepOnResult": true,
-      "vibrateOnResult": true,
-      "blinkAnimationOnResult": true,
       "cancelOnResult": true,
-      "reportingEnabled": true,
-      "segment": {
-        "titles": ["4 Digits", "5 Digits", "6 Digits"],
-        "modes": ["HEAT_METER_4", "HEAT_METER_5", "HEAT_METER_6"],
-        "tintColor": "CCCCCC",
-        "offset": {
-          "x": 0,
-          "y": 500
-        }
-      }
     },
-    {"nativeBarcodeEnabled": true}
-  ],
+    "nativeBarcodeEnabled": true,
+    "segment": {
+      "titles": ["Analog", "Digital"],
+      "modes": ["ANALOG_METER", "DIGITAL_METER"],
+      "tintColor": "CCCCCC",
+      "offset": {
+        "x": 0,
+        "y": 400
+      }
+    }
+  },
 
-  dialConfig: [
-    "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
-    {
-      "captureResolution": "1080p",
+
+  //  energyConfigWithSegment: [
+  //    "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
+  //    {
+  //      "captureResolution": "1080p",
+  //      "visualFeedback": {
+  //        "style": "CONTOUR_RECT",
+  //        "strokeColor": "0099FF",
+  //        "fillColor": "220099FF"
+  //      },
+  //      "cutout": {
+  //        "style": "rect",
+  //        "alignment": "top_half",
+  //        "strokeWidth": 2,
+  //        "cornerRadius": 4,
+  //        "strokeColor": "FFFFFF",
+  //        "outerColor": "000000",
+  //        "outerAlpha": 0.3,
+  //        "feedbackStrokeColor": "0099FF",
+  //      },
+  //      "flash": {
+  //        "mode": "manual",
+  //        "alignment": "bottom_right"
+  //      },
+  //      "beepOnResult": true,
+  //      "vibrateOnResult": true,
+  //      "blinkAnimationOnResult": true,
+  //      "cancelOnResult": true,
+  //      "reportingEnabled": true,
+  //      "segment": {
+  //        "titles": ["Analog", "Digital"],
+  //        "modes": ["ANALOG_METER", "DIGITAL_METER"],
+  //        "tintColor": "CCCCCC",
+  //        "offset": {
+  //          "x": 0,
+  //          "y": 400
+  //        }
+  //      }
+  //    },
+  //    {"nativeBarcodeEnabled": true}
+  //  ],
+
+  //no heat anymore as anyline4 does not support heater meters
+  //  heatConfigWithSegment: [
+  //    "eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJkZWJ1Z1JlcG9ydGluZyI6ICJvcHQtb3V0IiwgImlvc0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0sICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsICJtYWpvclZlcnNpb24iOiAiNCIsICJtYXhEYXlzTm90UmVwb3J0ZWQiOiAwLCAicGluZ1JlcG9ydGluZyI6IHRydWUsICJwbGF0Zm9ybSI6IFsgImlPUyIsICJBbmRyb2lkIiwgIldpbmRvd3MiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogZmFsc2UsICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwgInRvbGVyYW5jZURheXMiOiA5MCwgInZhbGlkIjogIjIwMjAtMTAtMjAiLCAid2luZG93c0lkZW50aWZpZXIiOiBbICJpby5hbnlsaW5lLmV4YW1wbGVzLmNvcmRvdmEiIF0gfQpJYzVHSWVpdTBUYmJoQjE4T2poeHllY1g3Q296NWorR1o2azVtanJTUUtxVFYrYWRKODk4MHA2QmZ6UVdoK1ZyCnF6UE4yTURuWnFNSTcwUk13NHFGV0VJek16Z1J2ZUg3ZzhYM3RHbUcyUTdzazh0Y1Q1Zk5aditNNmpTeXQ1WG4KM010Ry9yZnp2YVRiQlo5VnV5ektsVXdDakZVdVhqd2xIVm1QZS9hc2ljMkVpbWhMU2JTam9PN0Nzajhjd0ZNVApKZDJTTnBncmdQYUtSUzZrdlNFMEJJU3ltVnAvb1VIcm9xUGtlUWRxa2owQk1ZU3Z4VmM4L0p3L1RvdHNvY1IvCmxIWi93VG03UldGRDVhZXpIdjJDcjNVN1ArSW1KdkNUb3JCc3VUa3B6VzF1dHIvQlNkckI3dVJNVFpPOW84UjcKS1ZhaUlmNmZYSExQanBkbkpmQXdqUT09Cg==",
+  //    {
+  //      "captureResolution": "1080p",
+  //      "visualFeedback": {
+  //        "style": "RECT",
+  //        "strokeColor": "0099FF",
+  //        "fillColor": "220099FF"
+  //      },
+  //      "cutout": {
+  //        "style": "rect",
+  //        "alignment": "top_half",
+  //        "strokeWidth": 2,
+  //        "cornerRadius": 4,
+  //        "strokeColor": "FFFFFF",
+  //        "outerColor": "000000",
+  //        "outerAlpha": 0.3,
+  //        "feedbackStrokeColor": "0099FF",
+  //      },
+  //      "flash": {
+  //        "mode": "manual",
+  //        "alignment": "bottom_right"
+  //      },
+  //      "beepOnResult": true,
+  //      "vibrateOnResult": true,
+  //      "blinkAnimationOnResult": true,
+  //      "cancelOnResult": true,
+  //      "reportingEnabled": true,
+  //      "segment": {
+  //        "titles": ["4 Digits", "5 Digits", "6 Digits"],
+  //        "modes": ["HEAT_METER_4", "HEAT_METER_5", "HEAT_METER_6"],
+  //        "tintColor": "CCCCCC",
+  //        "offset": {
+  //          "x": 0,
+  //          "y": 500
+  //        }
+  //      }
+  //    },
+  //    {"nativeBarcodeEnabled": true}
+  //  ],
+
+  dialConfig:
+  {
+    "camera": {
+      "captureResolution": "720p"
+    },
+    "flash": {
+      "mode": "manual",
+      "alignment": "top_left",
+    },
+    "viewPlugin": {
+      "plugin": {
+        "id": "Meter_ID",
+        "meterPlugin": {
+          "scanMode": "DIGITAL_METER"
+        }
+      },
       "cutout": {
         "style": "rect",
         "maxWidthPercent": "90%",
@@ -221,34 +329,30 @@ meterViewPluginConfig: {
         "outerColor": "000000",
         "outerAlpha": 0.3
       },
-      "flash": {
-        "mode": "manual",
-        "alignment": "top_left"
+      "scanFeedback": {
+        "blinkOnResult": true,
+        "beepOnResult": true,
+        "vibrateOnResult": true
       },
-      "beepOnResult": true,
-      "vibrateOnResult": true,
-      "blinkAnimationOnResult": true,
-      "cancelOnResult": true
+      "cancelOnResult": true,
     },
-    {
-      "nativeBarcodeEnabled": true
-    }
-  ],
+    "nativeBarcodeEnabled": true
+  },
 
   scan: function (scanMode) {
-      
+
     if (localStorage.getItem("hasStartedAnyline") === 'true') {
       return;
     }
     localStorage.setItem("hasStartedAnyline", true);
 
-    this.meterViewPluginConfig.viewPlugin.plugin.scanMode = scanMode;
-      
+    this.meterViewPluginConfig.viewPlugin.plugin.meterPlugin.scanMode = scanMode;
+
     console.log("start scan with mode " + scanMode);
-    
+
     if (scanMode == 'SERIAL_NUMBER') {
-        this.meterViewPluginConfig.viewPlugin.plugin.serialNumberCharWhitelist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        this.meterViewPluginConfig.viewPlugin.plugin.serialNumberValidationRegex = '^[0-9A-Z]{5,}$';
+      this.meterViewPluginConfig.viewPlugin.plugin.meterPlugin.serialNumberCharWhitelist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      this.meterViewPluginConfig.viewPlugin.plugin.meterPlugin.serialNumberValidationRegex = '^[0-9A-Z]{5,}$';
     }
 
     console.log("your message here" + this.meterViewPluginConfig);
@@ -260,17 +364,30 @@ meterViewPluginConfig: {
     cordova.exec(this.onResult, this.onError, "AnylineSDK", "scan", [this.licenseKey, this.meterViewPluginConfig]);
   },
 
-  scanElectricDigitalSegment: function (scanMode) {
+  scanElectricAnalogSegment: function () {
     if (localStorage.getItem("hasStartedAnyline") === 'true') {
       return;
     }
     localStorage.setItem("hasStartedAnyline", true);
-
     // start the Energy scanning for the given scan mode
     // pass the success and error callbacks, as well as the license key and the config to the plugin
     // see http://documentation.anyline.io/#anyline-config for config details
     // and http://documentation.anyline.io/#energy for energy-module details
-    cordova.exec(this.onResult, this.onError, "AnylineSDK", scanMode, this.energyConfigWithSegment);
+    cordova.exec(this.onResult, this.onError, "AnylineSDK", "scan", [this.licenseKey, this.energyAnalogConfigWithSegment]);
+    // cordova.exec(this.onResult, this.onError, "AnylineSDK", scanMode, this.energyConfigWithSegment);
+  },
+
+  scanElectricDigitalSegment: function () {
+    if (localStorage.getItem("hasStartedAnyline") === 'true') {
+      return;
+    }
+    localStorage.setItem("hasStartedAnyline", true);
+    // start the Energy scanning for the given scan mode
+    // pass the success and error callbacks, as well as the license key and the config to the plugin
+    // see http://documentation.anyline.io/#anyline-config for config details
+    // and http://documentation.anyline.io/#energy for energy-module details
+    cordova.exec(this.onResult, this.onError, "AnylineSDK", "scan", [this.licenseKey, this.energyDigitalConfigWithSegment]);
+    // cordova.exec(this.onResult, this.onError, "AnylineSDK", scanMode, this.energyConfigWithSegment);
   },
 
   scanHeatMeterWithSegment: function () {
@@ -295,6 +412,6 @@ meterViewPluginConfig: {
     // pass the success and error callbacks, as well as the license key and the config to the plugin
     // see http://documentation.anyline.io/#anyline-config for config details
     // and http://documentation.anyline.io/#energy for energy-module details
-    cordova.exec(this.onResult, this.onError, "AnylineSDK", "SERIAL_NUMBER", this.serialNumberConfig);
+    cordova.exec(this.onResult, this.onError, "AnylineSDK", "scan", this.serialNumberConfig);
   },
 };
