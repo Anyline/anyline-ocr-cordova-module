a1e9a9d4c786ca7eb1688addfe6462be6a091c83
Jonas <jonas@anyline.io>
2019-05-13 15:46:55 +0200

    added disable App for Core Check

--- example/www/js/anyline.general.js
+++ example/www/js/anyline.general.js
@@ -7,12 +7,17 @@
 
 
 setTimeout(function () {
+    localStorage.setItem("hasStartedAnyline", true);
     cordova.exec(
         (sdkVersion) => {
+            localStorage.setItem("hasStartedAnyline", false);
             var div = document.getElementById('SDK');
             div.innerHTML = "<p>Anyline SDK Version: " + sdkVersion + "</p>"
         },
-        function (err) { console.error(err) },
+        function (err) {
+            localStorage.setItem("hasStartedAnyline", false);
+            console.error(err)
+        },
         "AnylineSDK",
         "GET_SDK_VERSION"
     );
