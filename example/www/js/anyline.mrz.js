c970345231257c7c09ad9e21126f71d8cfb5449e
Philipp Müller <philipp@anyline.io>
2020-09-23 21:14:23 +0200

    Bumped Plugin&Example version //TODO: maven update

--- example/www/js/anyline.mrz.js
+++ example/www/js/anyline.mrz.js
@@ -94,65 +94,69 @@ onError: function (error) {
 },
     
 anylineMRZViewConfig: {
-    "camera" : {
-        "captureResolution" : "1080p"
+    "camera": {
+      "captureResolution": "1080p"
     },
-    "flash" : {
-        "mode" : "manual",
-        "alignment" : "bottom_left"
+    "flash": {
+      "mode": "manual",
+      "alignment": "bottom_left"
     },
     "viewPlugin" : {
-        "plugin" : {
-            "id" : "IDPlugin_ID",
-            "idPlugin" : {
-                "mrzConfig" : {
-                    "strictMode" : false,
-                    "cropAndTransformID" : false,
-                    "mrzFieldScanOptions": {
-                        "vizDateOfIssue" : "optional",
-                        "vizAddress" : "optional"
-                    }
-                }
-            }
+      "plugin" : {
+          "id" : "IDPlugin_ID",
+          "idPlugin" : {
+              "mrzConfig" : {
+                "strictMode" : false,
+                 "cropAndTransformID" : false,
+                 "mrzFieldScanOptions": {
+                   "vizAddress" : "default",
+                   "vizDateOfIssue" : "default",
+                   "vizSurname" : "default",
+                   "vizGivenNames" : "default",
+                   "vizDateOfBirth" : "default",
+                   "vizDateOfExpiry" : "default",
+                  }
+              }
+          }
+      },
+      "cutoutConfig": {
+        "style": "rect",
+        "maxWidthPercent": "90%",
+        "maxHeightPercent": "90%",
+        "width" : 972,
+        "alignment": "center",
+        "ratioFromSize": {
+          "width": 125,
+          "height": 85
         },
-        "cutoutConfig" : {
-            "style": "rect",
-            "maxWidthPercent": "90%",
-            "maxHeightPercent": "90%",
-            "alignment": "center",
-            "ratioFromSize": {
-                "width": 125,
-                "height": 85
-            },
-            "strokeWidth": 2,
-            "cornerRadius": 4,
-            "strokeColor": "FFFFFF",
-            "outerColor": "000000",
-            "outerAlpha": 0.3,
-            "cropPadding": {
-                "x": 0,
-                "y": 0
-            },
-            "cropOffset": {
-                "x": 0,
-                "y": 0
-            },
-            "feedbackStrokeColor": "0099FF",
-            "offset": {
-                "x": 0,
-                "y": 30
-            }
+        "strokeWidth": 2,
+        "cornerRadius": 4,
+        "strokeColor": "FFFFFF",
+        "outerColor": "000000",
+        "outerAlpha": 0.3,
+        "cropPadding": {
+          "x": -30,
+          "y": -90
         },
-        "scanFeedback" : {
-            "style": "rect",
-            "strokeColor": "0099FF",
-            "strokeWidth": 2,
-            "blinkOnResult": true,
-            "beepOnResult": true,
-            "vibrateOnResult": true
+        "cropOffset": {
+          "x": 0,
+          "y": 90
         },
-        "cancelOnResult" : true
-        
+        "feedbackStrokeColor": "0099FF",
+        "offset": {
+          "x": 0,
+          "y": 30
+        }
+      },
+      "scanFeedback": {
+        "style": "rect",
+        "strokeColor": "0099FF",
+        "strokeWidth": 2,
+        "blinkOnResult": true,
+        "beepOnResult": true,
+        "vibrateOnResult": true
+      },
+      "cancelOnResult": true
     },
     "cropAndTransformErrorMessage": "Edges are not detected"
 },
