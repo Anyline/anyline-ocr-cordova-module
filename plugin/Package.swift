bde5869b331f0e80d0dc56b3f106fb1371c0f8ae
Balazs Farkas <balazs.farkas@anyline.com>
2026-05-08 12:22:38 +0200

    Release 56.0.2

--- plugin/Package.swift
+++ plugin/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Anyline SDK dependency
         .package(
             url: "https://github.com/Anyline/anyline-ocr-spm-module.git",
-            from: "56.0.0"
+            from: "56.0.1"
         )
     ],
     targets: [
